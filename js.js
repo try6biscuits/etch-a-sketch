a8dd317438e9f3409b2e3071c494133ef7fe7a5d
Brandon Ngo <Brandon.ngo.inbox@gmail.com>
2024-01-18 17:12:17 -0600

    js.js

--- js.js
+++ js.js
@@ -3,12 +3,12 @@ const container = document.querySelector('#container');
 function makeDivRows(numDiv) {
     for (let d = 0; d < numDiv; d++) {
         let row = document.createElement('div');
-
+        row.classList.add('row');
         for (let c = 0; c < 16; c++) { 
             let cell = document.createElement('div');
+            cell.classList.add('cell');
             row.appendChild(cell);
         }
-        
         container.appendChild(row);
     }
 }
@@ -16,3 +16,4 @@ function makeDivRows(numDiv) {
 makeDivRows(16); 
 
 
+// For the first row, the reason why they are all one color with only one mouse is hovering is due to them being the container class and not div 
