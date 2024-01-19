496a46693fb63bbeda3456fc7b09e050c3a3046a
Brandon Ngo <Brandon.ngo.inbox@gmail.com>
2024-01-19 13:50:50 -0600

    add prompt function

--- js.js
+++ js.js
@@ -12,10 +12,16 @@ makeDivCells(256);
 
 const cells = document.querySelectorAll('.cell')
 cells.forEach(cell => {
+    // You need to understand this after you are finished
     cell.addEventListener("mouseenter", (event) => {
         event.target.style.background = 'green' 
         // event.target.classList.add('green-cell')
         // Potential Future Changes
     });
   });
-  
+
+  function promptFunction() {
+    prompt("Change the grid size:");
+  }
+  const btn = document.querySelector('.changeSize')
+  btn.addEventListener('click', promptFunction);
