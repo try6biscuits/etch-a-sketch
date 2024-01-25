65459c52e20dd2f790ef1c0525c92c8f4febc017
Brandon Ngo <Brandon.ngo.inbox@gmail.com>
2024-01-25 10:50:12 -0600

    add class name for rows and cells

--- js.js
+++ js.js
@@ -2,10 +2,12 @@ const container = document.querySelector("#container");
 function makeGrid(rows, cols) {
   for (let r = 0; r < rows; r++) {
     const row = document.createElement('div'); // Create a row element
+    row.classList.add('rows')
     container.appendChild(row); // Append the row to the container
 
     for (let c = 0; c < cols; c++) {
       const cell = document.createElement('div'); // Create a cell div
+      cell.classList.add('cells')
       row.appendChild(cell); // Append the cell to the current row
     }
   }
